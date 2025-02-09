4a41c33393f7380226dfdb3b33b45a1ba02aba56
Nanda Darsi <nandakishore.darsi@gmail.com>
2025-02-09 16:25:47 -0600

    Update to day 1

---
+++ exercises/day1/exercise5.py
@@ -0,0 +1,9 @@
+# Exercise5: Write a program to concatenate two strings.
+
+# Create two variables firstName and lastName and assign them values
+firstName = "John"
+lastName = "Doe"
+
+# Concatenate firstName and lastName and assign it to fullName
+
+# Print fullName
